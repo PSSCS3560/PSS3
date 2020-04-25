42bd702f245282b1ca3245e6de883d25d6b11b7b
unknown <jasonyeh@cpp.edu>
2020-04-25 13:16:41 -0700

    First push

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import org.json.JSONObject;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
